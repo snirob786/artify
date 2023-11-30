bb1133fbbb0b620ac0231ffd76a1b0ac42230633
NirobTheDoodleInc <150327879+NirobTheDoodleInc@users.noreply.github.com>
2023-11-30 18:26:25 +0600

    feat: registration and login complete with payload

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv"
 import path from "path"
+import payload from "payload"
+import { InitOptions } from "payload/config"
 
 dotenv.config({
     path: path.resolve(__dirname, "../.env")
@@ -15,4 +17,27 @@ if (!cached) {
 interface Args {
     initOptions?: Partial<InitOptions>
 }
-export const getPayloadClient = async ({ initOption }: Args) => { }
+export const getPayloadClient = async ({ initOptions }: Args = {}) => {
+    if (!process.env.PAYLOAD_SECRET) {
+        throw new Error('PAYLOAD_SECRET is missing')
+    }
+
+    if (cached.client) {
+        return cached.client
+    }
+
+    if (!cached.promise) {
+        cached.promise = payload.init({
+            secret: process.env.PAYLOAD_SECRET,
+            local: initOptions?.express ? false : true,
+            ...(initOptions || {})
+        })
+    }
+
+    try {
+        cached.client = await cached.promise
+    } catch (error) {
+        cached.promise = null
+        throw error
+    }
+}
